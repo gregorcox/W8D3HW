42ba2a7156a0c3ebba7ee31e9aa381b92a13d57f
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-11 15:31:30 +0100

    adds mentor class

--- src/main/java/models/Mentor.java
+++ src/main/java/models/Mentor.java
@@ -1,4 +1,47 @@
 package models;
 
+import javax.persistence.*;
+
 public class Mentor {
+
+    private int id;
+    private String name;
+    private Student student;
+
+    public Mentor(String name){
+        this.name = name;
+    }
+
+    public Mentor(){
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
