8d67f2f969df5bca2d0cf76a3adedb0e80dcc4a7
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-11 15:28:41 +0100

    adds lesson class

--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,4 +1,70 @@
 package models;
 
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Fetch;
+
 public class Lesson {
+
+    private int id;
+    private String title;
+    private int classroomNumber;
+    private Instructor instructor;
+    private Course course;
+
+    public Lesson(String title, int classroomNumber){
+        this.title = title;
+        this.classroomNumber = classroomNumber;
+    }
+
+    public Lesson(){
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    @Column(name = "classroomNumber")
+    public int getClassroomNumber() {
+        return classroomNumber;
+    }
+
+    @OneToOne(mappedBy = "lesson", fetch = FetchType.LAZY)
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setClassroomNumber(int classroomNumber) {
+        this.classroomNumber = classroomNumber;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
