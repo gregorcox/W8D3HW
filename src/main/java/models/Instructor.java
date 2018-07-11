1dd8118a369ed75cff15072ef68e7766a020f649
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-11 15:35:17 +0100

    adds instructor class

--- src/main/java/models/Instructor.java
+++ src/main/java/models/Instructor.java
@@ -1,4 +1,47 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+
 public class Instructor {
+
+    private int id;
+    private String name;
+    private List<Lesson> lessons;
+
+    public Instructor(String name){
+        this.name = name;
+    }
+
+    public Instructor(){
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY)
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
 }
